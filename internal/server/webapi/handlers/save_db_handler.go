e4e7713a3064ef0e4d85954a9c16b92a30a18513
lumpsoid <mmgeeki@gmail.com>
2024-05-02 13:58:59 +0200

    feat: add Dockerfile and docker-compose.yaml

--- internal/server/webapi/handlers/save_db_handler.go
+++ internal/server/webapi/handlers/save_db_handler.go
@@ -12,7 +12,7 @@ var SaveDb = server.Get("/billdb-save", func(s *server.Server) echo.HandlerFunc 
 	return func(c echo.Context) error {
     timestamp := time.Now().Local().Format("20060102")
     return c.Attachment(
-      s.Config.Db.Path, 
+      s.Config.DbPath, 
       fmt.Sprintf("billdb%s.db", timestamp),
     )
 	}
