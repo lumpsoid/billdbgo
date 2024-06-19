417943312b657eb14c2deaa47bdfb944a4abf5d9
lumpsoid <mmgeeki@gmail.com>
2024-06-19 18:55:26 +0200

    feat: bills browsing page

--- internal/server/webapi/handlers/bill_browse_handler.go
+++ internal/server/webapi/handlers/bill_browse_handler.go
@@ -2,28 +2,64 @@ package handlers
 
 import (
 	"billdb/internal/server"
+	"fmt"
 	"net/http"
+	"strconv"
+	"time"
 
 	"github.com/labstack/echo/v4"
 )
 
 var (
-	BillBrowse = server.Get("/bills", func(s *server.Server) echo.HandlerFunc {
+	BillBrowseLanding = server.Get("/bills", func(s *server.Server) echo.HandlerFunc {
 		return func(c echo.Context) error {
+			timeNow := time.Now()
+			return c.Redirect(http.StatusMovedPermanently, "/bills/"+timeNow.Format("2006/01"))
+		}
+	})
+
+	BillBrowse = server.Get("/bills/:y/:m", func(s *server.Server) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			r := make(map[string]interface{})
+			r["success"] = false
+
+			year, err := strconv.ParseInt(c.Param("y"), 10, 64)
+			if err != nil {
+				r["message"] = fmt.Sprintf("Invalid year: %s | URL: %s", c.Param("y"), c.Request().URL)
+				return c.Render(http.StatusOK, "bills-browse.html", r)
+			}
+			month, err := strconv.ParseInt(c.Param("m"), 10, 64)
+			if err != nil {
+				r["message"] = fmt.Sprintf("Invalid month: %s | URL: %s", c.Param("m"), c.Request().URL)
+				return c.Render(http.StatusOK, "bills-browse.html", r)
+			}
+
+			timeNow := time.Now()
+			timeRequested := time.Date(int(year), time.Month(month), 1, 0, 0, 0, 0, time.UTC)
+			if timeRequested.After(timeNow) {
+				r["message"] = "Requested date is in the future"
+				return c.Render(http.StatusOK, "bills-browse.html", r)
+			}
+
+			queryBase := `
+      SELECT
+				id, name, dates, price, currency, exchange_rate, country, tag
+			FROM
+				bills
+			WHERE
+				strftime('%%Y-%%m', dates) = '%d-%02d'
+			ORDER BY
+				dates DESC;
+      `
+			query := fmt.Sprintf(queryBase, year, month)
 
 			db := s.BillRepo.GetDb()
-
-			query := `
-      SELECT
-        id, name, dates, price, currency, exchange_rate, country, tag
-      FROM bills
-      ORDER BY dates DESC
-      LIMIT 20;
-      `
 			rows, err := db.Query(query)
 			if err != nil {
-				return err
+				r["message"] = "Error while querying the database"
+				return c.Render(http.StatusOK, "bills-browse.html", r)
 			}
+			defer rows.Close()
 
 			var billsResponse []Bill
 			for rows.Next() {
@@ -39,7 +75,8 @@ var (
 				)
 				rows.Scan(&Id, &Name, &Date, &Price, &Currency, &ExchangeRate, &Country, &Tag)
 				if err != nil {
-					return err
+					r["message"] = "Error while scanning the database"
+					return c.Render(http.StatusOK, "bills-browse.html", r)
 				}
 				billsResponse = append(billsResponse, Bill{
 					Id:           Id,
@@ -53,9 +90,16 @@ var (
 				})
 			}
 
-			return c.Render(http.StatusOK, "bills-browse.html", map[string]interface{}{
-				"bills": billsResponse,
-			})
+			nextMonth := timeRequested.AddDate(0, 1, 0)
+			if nextMonth.Before(timeNow) {
+				r["nextPage"] = c.Echo().Reverse("bills", nextMonth.Year(), int(nextMonth.Month()))
+			}
+			prevMonth := timeRequested.AddDate(0, -1, 0)
+			r["prevPage"] = c.Echo().Reverse("bills", prevMonth.Year(), int(prevMonth.Month()))
+
+			r["success"] = true
+			r["bills"] = billsResponse
+			return c.Render(http.StatusOK, "bills-browse.html", r)
 		}
 	})
 )
