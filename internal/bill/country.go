347b13288860a2c913af9a3a95a5357548ad5a8c
lumpsoid <mmgeeki@gmail.com>
2024-06-24 18:09:15 +0200

    refactor: country system

--- internal/bill/country.go
+++ internal/bill/country.go
@@ -5,23 +5,32 @@ import "fmt"
 type Country int
 
 const (
-	SERBIA Country = iota
+	SERBIA Country = iota // int 0
 	TURKEY
 	RUSSIA
 )
 
-// Map string values to enum values
-var countryMap = map[string]Country{
-	"serbia": SERBIA,
-	"turkiye": TURKEY,
-	"russia": RUSSIA,
-}
-
+// must align with the Country enum
 var countryToString = []string{"serbia", "turkey", "russia"}
 
-func StringToCountry(s string) (Country, error) {
-	if country, ok := countryMap[s]; ok {
-		return country, nil
+func (c Country) String() string {
+	return countryToString[c]
+}
+
+func StringToCountry(countryString string) (Country, error) {
+	switch countryString {
+	case "serbia":
+		return SERBIA, nil
+	case "turkey":
+		return TURKEY, nil
+	case "russia":
+		return RUSSIA, nil
+	default:
+		return -1, fmt.Errorf("Country %s not found", countryString)
 	}
-	return -1, fmt.Errorf("Country %s not found", s)
+}
+
+func GetCountryList() []string {
+	countryList := append([]string{}, countryToString...)
+	return countryList
 }
---
+++ internal/bill/country_test.go
@@ -0,0 +1,18 @@
+package bill
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestCountryToString(t *testing.T) {
+	fmt.Println("TestCountryToString")
+	country := SERBIA
+	if country.String() != "serbia" {
+		t.Errorf("Currency as string: `%s`, expected `rsd`", country)
+	}
+	country = TURKEY
+	if country.String() != "turkey" {
+		t.Errorf("Currency as string: `%s`, expected `rub`", country)
+	}
+}
